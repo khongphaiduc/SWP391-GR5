8af6348df424ebb61977a46faeb15fc247be3ebe
khongphaiduc <you@example.com>
2025-05-18 20:54:08 +0700

    'changeDBcontext'

--- Project_RecruitmentWebsite/src/java/dal/DBContext.java
+++ Project_RecruitmentWebsite/src/java/dal/DBContext.java
@@ -14,7 +14,7 @@ public class DBContext {
         try {
             String user = "sa";
             String pass = "123";
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=Y24SP1B5";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=thaycaiurlsqlnay";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
