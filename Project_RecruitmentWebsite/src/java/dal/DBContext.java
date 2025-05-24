abcc64fc95dc1023b9a32fb6c35dd6b7f6e04738
khongphaiduc <ptrungduc1011@gmail.com>
2025-05-24 13:43:43 +0700

    Update DBContext.java

--- Project_RecruitmentWebsite/src/java/dal/DBContext.java
+++ Project_RecruitmentWebsite/src/java/dal/DBContext.java
@@ -14,7 +14,7 @@ public class DBContext {
         try {
             String user = "sa";
             String pass = "123";
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=CV1";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=CV";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
