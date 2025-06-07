37153d5973ef59ab51456dddad450bd4d2e75044
PhamTrungDuc <you@example.com>
2025-06-07 23:13:07 +0700

    Feature Allow User Send Feedback to Admin Team

--- Project_RecruitmentWebsite/src/java/dal/DBContext.java
+++ Project_RecruitmentWebsite/src/java/dal/DBContext.java
@@ -20,7 +20,7 @@ public class DBContext {
 
 
            // String url = "jdbc:sqlserver://DESKTOP-NQH197\\NGUYENQUANGHUYSV:1433;databaseName=swp_final4;";
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=TOPCV13";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=GenZTimViec1";
 
 
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
