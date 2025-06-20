f935109247ec692403de36d0fc88abac38573e43
Minh Khánh <minhkhanhs204@gmail.com>
2025-06-20 23:21:31 +0700

    review iter1 <thieu pagingtion>

--- Project_RecruitmentWebsite/src/java/dal/DBContext.java
+++ Project_RecruitmentWebsite/src/java/dal/DBContext.java
@@ -16,21 +16,15 @@ public class DBContext {
             String user = "sa";
             String pass = "123";
 
-
-
-   //         String url = "jdbc:sqlserver://DESKTOP-NQH197\\NGUYENQUANGHUYSV:1433;databaseName=swp_final6;";
-
-
-
-
+            //         String url = "jdbc:sqlserver://DESKTOP-NQH197\\NGUYENQUANGHUYSV:1433;databaseName=swp_final6;";
 //            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=GenZTimViec9";
-            String url = "jdbc:sqlserver://LAPTOP-MK:1433;databaseName=CV03";
+            String url = "jdbc:sqlserver://LAPTOP-MK:1433;databaseName=CV04";
 
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
 //             String url = "jdbc:sqlserver://10.33.61.112:1433;databaseName=GenZTimViec14;user=sa;password=123";
 //           Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//>>>>>>> b7aaaaddf37a469d9faf74f33f90c10c8c2134b2
+
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
